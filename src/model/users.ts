9a4cef31bcd16ae2ebe9549b60e420a95791bd0e
naphattharawat <naphattharawat@gmail.com>
2021-11-15 16:39:12 +0700

    users

--- src/model/users.ts
+++ src/model/users.ts
@@ -4,4 +4,26 @@ export class UsersModel {
     getList(db: Knex) {
         return db('users')
     }
+
+    saveUser(db: Knex, data: any) {
+        return db('users')
+            .insert(data);
+    }
+
+    info(db: Knex, id: number) {
+        return db('users')
+            .where('id', id)
+    }
+
+    delete(db: Knex, id: number) {
+        return db('users')
+            .where('id', id)
+            .delete()
+    }
+
+    update(db: Knex, id: number, data: any) {
+        return db('users')
+            .where('id', id)
+            .update(data);
+    }
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -10,10 +10,55 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', async function (req: Request, res: Response, next: NextFunction) {
     try {
-        const rs:any = await usersModel.getList(req.db);
-        res.send({ok:true,rows:rs})
+        const rs: any = await usersModel.getList(req.db);
+        res.send({ ok: true, rows: rs })
     } catch (error) {
-        res.send({ok:false,error:error})
+        res.send({ ok: false, error: error })
+    }
+});
+
+router.get('/info', async function (req: Request, res: Response, next: NextFunction) {
+    try {
+        const id: any = req.query.id;
+        const rs: any = await usersModel.info(req.db, +id);
+        res.send({ ok: true, rows: rs })
+    } catch (error) {
+        res.send({ ok: false, error: error })
+    }
+});
+
+router.post('/', async function (req: Request, res: Response, next: NextFunction) {
+    try {
+        const body = req.body;
+        if (body.first_name && body.last_name) {
+            const rs: any = await usersModel.saveUser(req.db, body);
+            res.send({ ok: true, rows: rs })
+        } else {
+            res.send({ ok: false, error: 'ไม่พบ parameter' })
+        }
+    } catch (error) {
+        res.send({ ok: false, error: error })
+    }
+});
+
+router.delete('/:id', async function (req: Request, res: Response, next: NextFunction) {
+    try {
+        const id: any = req.params.id;
+        const rs: any = await usersModel.delete(req.db, +id);
+        res.send({ ok: true, rows: rs })
+    } catch (error) {
+        res.send({ ok: false, error: error })
+    }
+});
+
+router.put('/:id', async function (req: Request, res: Response, next: NextFunction) {
+    try {
+        const id: any = req.params.id;
+        const body: any = req.body;
+        const rs: any = await usersModel.update(req.db, +id, body);
+        res.send({ ok: true, rows: rs })
+    } catch (error) {
+        res.send({ ok: false, error: error })
     }
 });
 
