af4c568d89f1c3e60bbf2685f1ccd3aa2bbbe845
beerIT15kmutnb <beerde789@hotmail.com>
2021-11-16 11:20:29 +0700

    join

--- src/model/users.ts
+++ src/model/users.ts
@@ -2,9 +2,15 @@ import { Knex } from 'knex'
 export class UsersModel {
 
     getList(db: Knex) {
-        return db('users')
+        return db('users as u')
+        .select('u.id', 'u.first_name',
+        'u.last_name', 'u.title_id', 't.name as title_name')
+        .join('titles as t', 't.id', 'u.title_id')
     }
-
+    // select u.id,u.first_name,u.last_name,u.title_id,
+    // t.name as title_name 
+    // from users as u 
+    // join titles as t on t.id = u.title_id
     saveUser(db: Knex, data: any) {
         return db('users')
             .insert(data);
